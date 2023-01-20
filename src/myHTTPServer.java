402acf58201f7888483ac1b2e8a6889c4bace68e
slienhmjgvgerghrgewahrfvvnmjeige <77601091+slienhmjgvgerghrgewahrfvvnmjeige@users.noreply.github.com>
2023-01-20 01:47:26 -0800

    Impemented 206 response without chopping data

--- src/myHTTPServer.java
+++ src/myHTTPServer.java
@@ -40,6 +40,7 @@ public class myHTTPServer extends Thread {
             StringTokenizer tokenizer = new StringTokenizer(headerLine);
             String httpMethod = tokenizer.nextToken();
             String httpQueryString = tokenizer.nextToken();
+            String httpRange = "";
 
             StringBuffer responseBuffer = new StringBuffer();
             responseBuffer.append("<b> This is the HTTP Server Home Page.... </b><BR>");
@@ -48,6 +49,9 @@ public class myHTTPServer extends Thread {
             System.out.println("The HTTP request string is ....");
             while (inFromClient.ready())
             {
+                if (requestString.contains("Range:")){// Isolate range string
+                    httpRange = requestString;
+                }
                 // Read the HTTP complete HTTP Query
                 responseBuffer.append(requestString + "<BR>");
                 System.out.println(requestString);
@@ -57,28 +61,31 @@ public class myHTTPServer extends Thread {
             if (httpMethod.equals("GET")) {
                 if (httpQueryString.equals("/")) {
                     // The default home page
-                    sendResponse(200, responseBuffer.toString(), false);
-                } else {
-//This is interpreted as a file name
+                    sendResponse(200, responseBuffer.toString(), false, "");
+                } else { //This is interpreted as a file name
                     String fileName = httpQueryString.replaceFirst("/", "");
                     fileName = "content/" +URLDecoder.decode(fileName);
                     if (new File(fileName).isFile()){
-                        sendResponse(200, fileName, true);
-                    }
-                    else {
+                        if (httpRange != ""){ //Partial File request
+                            httpRange =  httpRange.split("=",2)[1];
+                            sendResponse(206, fileName, true, httpRange);
+                        } else { //Full file request
+                            sendResponse(200, fileName, true, "");
+                        }
+                    } else {
                         sendResponse(404, "<b>The Requested resource not found ...." +
-                                "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>", false);
+                                "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>", false, "");
                     }
                 }
             }
             else sendResponse(404, "<b>The Requested resource not found ...." +
-                    "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>", false);
+                    "Usage: http://127.0.0.1:5000 or http://127.0.0.1:5000/</b>", false, "");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void sendResponse (int statusCode, String responseString, boolean isFile) throws Exception {
+    public void sendResponse (int statusCode, String responseString, boolean isFile, String range) throws Exception {
 
         String statusLine = null;
         String serverdetails = "Server: Java HTTPServer";
@@ -88,10 +95,10 @@ public class myHTTPServer extends Thread {
         FileInputStream fin = null;
         ZonedDateTime date = ZonedDateTime.now();
 
-
-
         if (statusCode == 200)
             statusLine = "HTTP/1.1 200 OK" + "\r\n";
+        else if (statusCode == 206)
+            statusLine = "HTTP/1.1 206 Partial Content" + "\r\n";
         else
             statusLine = "HTTP/1.1 404 Not Found" + "\r\n";
 
@@ -116,20 +123,41 @@ public class myHTTPServer extends Thread {
         outToClient.writeBytes("Connection: Keep-Alive\r\n");
         outToClient.writeBytes("\r\n");
 
-        if (isFile) sendFile(fin, outToClient);
+        if (isFile) sendFile(fin, outToClient, range);
         else outToClient.writeBytes(responseString);
 
         outToClient.close();
     }
 
-    public void sendFile (FileInputStream fin, DataOutputStream out) throws Exception {
+    public void sendFile (FileInputStream fin, DataOutputStream out, String range) throws Exception {
         byte[] buffer = new byte[1024] ;
         int bytesRead;
 
-        while ((bytesRead = fin.read(buffer)) != -1 ) {
-            out.write(buffer, 0, bytesRead);
+        if (true){ //if (range.length() == 0) {
+            while ((bytesRead = fin.read(buffer)) != -1 ) {
+                out.write(buffer, 0, bytesRead);
+            }
+            fin.close();
         }
-        fin.close();
+        
+        /* else { //Cut the output into blocks
+            String[] ranges = range.split(",");
+            for(int i=0; i<ranges.length; i++){ //Loop through each range set
+                ranges[i] = ranges[i] + "-";
+                String[] endsString = ranges[i].split("-");
+                System.out.println(endsString[0]);
+
+                int[] ends = {0,0};
+                ends[0] += Integer.valueOf(endsString[0]); 
+                ends[1] += Integer.valueOf(endsString[1]);
+                System.out.println("ends" + ends[0]+" "+ends[1]);
+
+                while ((bytesRead = fin.read(buffer)) != -1 ) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+            fin.close();
+        }*/
     }
 
     public static void main (String args[]) throws Exception {
